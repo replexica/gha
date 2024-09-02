93cfc600c5db81cfe80d1011dab0b2421f14cbac
maxprilutskiy <maks.prilutskiy@gmail.com>
2024-09-02 16:06:28 +0200

    fix: gha

--- action/index.mjs
+++ action/index.mjs
@@ -5,9 +5,12 @@ console.log(process.env);
 const octokit = new Octokit({ auth: process.env.GH_TOKEN });
 
 // Create a new issue in the current repository
+const ownerName = process.env.GITHUB_REPOSITORY_OWNER;
+const fullRepoName = process.env.GITHUB_REPOSITORY;
+const repoName = fullRepoName.split('/')[1];
 await octokit.rest.issues.create({
-  owner: process.env.GITHUB_REPOSITORY_OWNER,
-  repo: process.env.GITHUB_REPOSITORY,
+  owner: ownerName,
+  repo: repoName,
   title: 'New Issue',
   body: 'This is a new issue created by the Replexica Action'
 });
