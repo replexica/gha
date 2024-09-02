8b2977091a0f38d554793b2dd905757d08809f9e
maxprilutskiy <maks.prilutskiy@gmail.com>
2024-09-02 16:01:33 +0200

    fix: js script

--- action/index.mjs
+++ action/index.mjs
@@ -2,14 +2,12 @@ import { App } from "octokit";
 
 console.log(process.env);
 
-const app = new App({
-  auth: process.env.GH_TOKEN,
-});
+const octokit = new Octokit({ auth: process.env.GH_TOKEN });
 
 // Create a new issue in the current repository
-await app.octokit.rest.issues.create({
-  owner: context.repo.owner,
-  repo: context.repo.repo,
+await octokit.rest.issues.create({
+  owner: process.env.GITHUB_REPOSITORY_OWNER,
+  repo: process.env.GITHUB_REPOSITORY_NAME,
   title: 'New Issue',
   body: 'This is a new issue created by the Replexica Action'
 });
