244591f167677890cbbb31dfe597b3d577f790b9
maxprilutskiy <maks.prilutskiy@gmail.com>
2024-09-02 15:59:33 +0200

    chore: upd

--- action/index.mjs
+++ action/index.mjs
@@ -1,9 +1,13 @@
-import { Octokit, App } from "octokit";
+import { App } from "octokit";
 
-const octokit = new Octokit({ auth: process.env.GH_TOKEN });
+console.log(process.env);
+
+const app = new App({
+  auth: process.env.GH_TOKEN,
+});
 
 // Create a new issue in the current repository
-await octokit.rest.issues.create({
+await app.octokit.rest.issues.create({
   owner: context.repo.owner,
   repo: context.repo.repo,
   title: 'New Issue',
