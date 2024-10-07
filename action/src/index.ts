14f1537752a9a138dd484fc7d5e93daf2224a423
maxprilutskiy <maks.prilutskiy@gmail.com>
2024-10-06 18:18:09 -0700

    upd

--- action/src/index.ts
+++ action/src/index.ts
@@ -11,23 +11,38 @@ import doStuff from './do-stuff.js';
   const config = await loadConfig();
   const octokit = await loadOctokit();
 
-  // Do stuff
-  await doStuff();
   // Commit changes
   execSync('git config --global user.name "Replexica"');
   execSync('git config --global user.email "support@replexica.com"');
   execSync(`git config --global safe.directory ${process.cwd()}`);
 
   if (!config.isPullRequestMode) {
+    // Do stuff
+    await doStuff();
     execSync('git add .');
     execSync(`git commit -m "${config.commitMessage}"`);
     execSync('git push');
   } else {
     // Calculate automated branch name
     const prBranchName = `replexica/${config.currentBranchName}`;
+    const branchExists = await octokit.rest.repos.getBranch({
+      owner: config.repositoryOwner,
+      repo: config.repositoryName,
+      branch: prBranchName,
+    }).then(({ status }) => status === 200);
 
-    // Create branch
-    execSync(`git checkout -b ${prBranchName}`);
+    if (branchExists) {
+      execSync(`git checkout ${prBranchName}`);
+      execSync('git pull');
+      // merge current branch into pr branch
+      execSync(`git merge ${config.currentBranchName}`);
+    } else {
+      // Create branch
+      execSync(`git checkout -b ${prBranchName}`);
+    }
+
+    // Do stuff
+    await doStuff();
     execSync('git add .');
     execSync(`git commit -m "${config.commitMessage}"`);
     execSync(`git push --set-upstream origin "${prBranchName}"`);
