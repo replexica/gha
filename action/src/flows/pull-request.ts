2ac33aa6b00cfb4d73d4f36e606e327b988a1b7a
maxprilutskiy <maks.prilutskiy@gmail.com>
2024-11-09 17:04:02 -0800

    chore: upd

--- action/src/flows/pull-request.ts
+++ action/src/flows/pull-request.ts
@@ -139,7 +139,7 @@ export class PullRequestFlow extends InBranchFlow {
 
   private getPrBodyContent(): string {
     return `
-Hey @${this.config.repositoryOwner} team,
+Hey team,
 
 [**Replexica AI**](https://replexica.com) here with fresh localization updates!
 
